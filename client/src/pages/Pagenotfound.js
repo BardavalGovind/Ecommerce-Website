b537545cfe96029326374693370580fd13f696db
Govind <bardavalgovind2005@gmail.com>
2025-01-22 11:45:42 +0530

    routing for different page is implemented

---
+++ client/src/pages/Pagenotfound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+const Pagenotfound = () => {
+  return (
+    <Layout>
+        <h1>Pagenot found</h1>
+    </Layout>
+  )
+}
+
+export default Pagenotfound
