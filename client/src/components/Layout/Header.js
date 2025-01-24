3f107f14754be117962742aca4320da8531cf0f1
Govind <bardavalgovind2005@gmail.com>
2025-01-24 11:57:57 +0530

    Context api implemented

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import {NavLink, Link} from 'react-router-dom';
-// import { RiShoppingBagFill } from "react-icons/ri";
 import { HiShoppingCart } from "react-icons/hi2";
+import { useAuth } from '../../context/auth';
+import toast from 'react-hot-toast';
 
 const Header = () => {
+  const [auth, setAuth] = useAuth();
+  const handleLogout = () => {
+    setAuth({
+      ...auth, user:null, token:''
+    })
+    localStorage.removeItem('auth');
+    toast.success("Logout Successfully");
+  }
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -33,16 +42,30 @@ const Header = () => {
                    Category
                   </NavLink>
               </li>
-              <li className="nav-item">
-                <NavLink to="/register" className="nav-link" href="#">
-                  Register
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink to="/login" className="nav-link" href="#">
-                  Login
-                </NavLink>
-              </li>
+              {
+                !auth.user ? (
+                  <>
+                  <li className="nav-item">
+                    <NavLink to="/register" className="nav-link" href="#">
+                      Register
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink to="/login" className="nav-link" href="#">
+                      Login
+                    </NavLink>
+                  </li>
+                  </>
+                ) : (
+                  <>
+                    <li className="nav-item">
+                      <NavLink onClick={handleLogout} to="/login" className="nav-link" href="#">
+                        Logout
+                      </NavLink>
+                    </li>
+                  </>
+                )
+              }
               <li className="nav-item">
                 <NavLink to="/cart" className="nav-link" href="#">
                   Cart (0)
